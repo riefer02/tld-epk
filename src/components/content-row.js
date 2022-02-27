6f98b14e5bb091a0a2ad688947f7f35c5338564c
riefer02 <andrew.riefenstahl@gmail.com>
2022-02-27 08:41:37 -0600

    add intersectionObserver to control lazy loading

--- src/components/content-row.js
+++ src/components/content-row.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useRef } from "react"
+import useIntersectionObserver from "../lib/observer"
 
 export default function ContentRow({ children, animation }) {
+  const ref = useRef(null)
+  const entry = useIntersectionObserver(ref, {})
+  const isVisible = !!entry?.isIntersecting
+
   return (
     <div
+      ref={ref}
       className="page-content__row"
       data-sal={animation}
       data-sal-delay="300"
       data-sal-easing="ease-in"
     >
-      {children}
+      {isVisible ? children : <div className="skeleton-loader"></div>}
     </div>
   )
 }
