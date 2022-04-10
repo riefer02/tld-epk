363bf58e1320df9b0f0527c052c66a6819cbc5ba
riefer02 <andrew.riefenstahl@gmail.com>
2022-04-10 12:32:14 -0500

    opacitize hamburger button

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -3,12 +3,13 @@ import React from "react"
 export default function Hamburger({ navOpen, setNavOpen }) {
   return (
     <div className="hamburger__container">
-      <button onClick={()=>setNavOpen(!navOpen)} className="hamburger__button">
-        <div
-          className={`hamburger__line-group ${
-            navOpen ? "hamburger--open" : "hamburger--closed"
-          }`}
-        >
+      <button
+        onClick={() => setNavOpen(!navOpen)}
+        className={`hamburger__button ${
+          navOpen ? "hamburger--open" : "hamburger--closed"
+        }`}
+      >
+        <div className="hamburger__line-group">
           <div className="hamburger__line"></div>
           <div className="hamburger__line"></div>
           <div className="hamburger__line"></div>
