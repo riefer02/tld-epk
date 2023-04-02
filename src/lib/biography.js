42d85efb7caf4a28888595d8a14c0fde4e9206cf
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-04-02 11:34:17 -0500

    typo fix

--- src/lib/biography.js
+++ src/lib/biography.js
@@ -2,7 +2,7 @@ export const biography = [
   "The Lewd Dudes are the #1 hottest alternative rock band formed in Austin, Texas in 2017 by Andrew Riefenstahl (Producer + Guitar), Riley Sklar (Bass), Rankin Fetzer (Guitar), Scott Morgan (Keys), and Jordan Hughes (Drums).",
   "The Lewd Dudes are a feel good rock and roll band, and over the course of six years, this ensemble has matured like aging wine.",
   "Latest Album: Pulling inspiration from their experiences and the collapse of live music during the pandemic, Habitable Zone sets you on a colossal interplanetary exploration of the human condition, self-reflection, mortality, failed relationships, over indulgance, bliss, and the hope for a new beginning.",
-  "Latest Single: The Lewd Dudes went on hiatus in the Summer of 2022, but wanted to leave on a high note by releasing their latest single 'Mad in Love', an story about the struggles of love and of course madness.",
+  "Latest Single: The Lewd Dudes went on brief hiatus in the Summer of 2022, but wanted to leave on a high note by releasing their latest single 'Mad in Love', a story about the struggles of love and of course madness.",
   `Update: The Lewd Dudes are playing Eeyore's Birthday April 29th, 2023 in Austin, Texas in support of the local Austin community with a message to "Keep Austin Weird"`,
 ]
 
