b4975e9028d9161e9666215b687288eede40775e
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-04-02 19:45:44 -0500

    update npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,22 +34,6 @@ module.exports = {
         icon: `src/images/favicon-tld-epk.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
-      options: {
-        threshold: 0.3, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        disable: false, // Flag for disabling animations
-
-        // Advanced Options
-        selector: "[data-sal]", // Selector of the elements to be animated
-        animateClassName: "sal-animate", // Class name which triggers animation
-        disabledClassName: "sal-disabled", // Class name which defines the disabled state
-        rootMargin: "0% 50%", // Corresponds to root's bounding box margin
-        enterEventName: "sal:in", // Enter event name
-        exitEventName: "sal:out", // Exit event name
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
